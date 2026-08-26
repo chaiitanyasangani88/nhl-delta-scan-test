b5f1ca9f17bb5b7bf7c4dc615ff5a927237ecd7e
chaiitanyasangani88 <chaitanyasangani88@gmail.com>
2026-08-26 12:45:05 +0530

    Add an order lookup endpoint

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const legacy = require('./src/legacy');
+const orders = require('./src/orders');
 
 const app = express();
 app.use(express.json());
 app.use('/legacy', legacy);
+app.use('/api', orders);
 
 app.listen(process.env.PORT || 3000);
 module.exports = app;
